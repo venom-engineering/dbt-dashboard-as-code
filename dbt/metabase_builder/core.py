41da5ccf7df2e69bb512d3d88d97c4e2ee878da4
axel_thevenot <axel_thevenot@ext.carrefour.com>
2025-02-25 17:43:01 +0100

    feat: metabase

---
+++ dbt/metabase_builder/core.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+from metabase.models import Metabase
+from metabase.hooks import MetabaseHook
+
+from dbt_utils import read_manifest, get_exposures, exposure_to_card
+
+METABASE_URL = os.environ.get('METABASE_URL', 'http://localhost:3000')
+METABASE_API_KEY = os.environ.get('METABASE_API_KEY', 'mb_eB0jYj/56l6HGbMWZ6ICfkytAKRnQFjBRRyBh9hsB2k=')
+
+metabase_hook = MetabaseHook(METABASE_URL, METABASE_API_KEY)
+metabase = Metabase(hook=metabase_hook)
+
+if __name__ == '__main__':
+    manifest = read_manifest(manifest_path='../target/manifest.json')
+
+    exposures = get_exposures(manifest=manifest)
+
+    for exposure in exposures:
+        card_data = exposure_to_card(
+            exposure=exposure,
+            metabase=metabase,
+            manifest=manifest,
+        )
+
+        print(json.dumps(card_data, indent=2))
+
+        if not card_data:
+            continue
+
+
+        metabase.hook.put('card/40', card_data)
+        # metabase.hook.post('card/', card_data)

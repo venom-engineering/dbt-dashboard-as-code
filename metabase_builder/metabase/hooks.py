6a5cd64bf0c6c9b67b15dc37bea9c60cff1c19df
axel_thevenot <axel_thevenot@ext.carrefour.com>
2025-02-27 17:29:25 +0100

    refactor: logic is moved from hook to metabase

--- metabase_builder/metabase/hooks.py
+++ metabase_builder/metabase/hooks.py
@@ -5,8 +5,6 @@ import requests
 from typing import List, Dict, Any
 
 
-from metabase.models import Database, Table, Field, Card
-
 class MetabaseHook:
 
     def __init__(
@@ -63,43 +61,3 @@ class MetabaseHook:
         except requests.exceptions.RequestException as e:
             print(f'Error retrieving objects from {endpoint}: {e}')
         return {}
-
-    def get_databases(self) -> Dict[int, Database]:
-        '''Lists databases in Metabase.'''
-        databases = [Database(**database) for database in self.get(endpoint='database')['data']]
-        databases = {database.id: database for database in databases}
-        return databases
-
-    def get_tables(self) -> Dict[int, Table]:
-        '''Lists tables in Metabase.'''
-        tables = [
-            table for table in self.get(endpoint='table')
-        ]
-        tables = [Table(**table) for table in tables]
-        tables = {table.id: table for table in tables}
-        return tables
-
-    def get_database_fields(self, database_id) -> Dict[int, Field]:
-        fields = [Field(**field) for field in self.get(endpoint=f'database/{database_id}/fields')]
-        fields = {field.id: field for field in fields}
-        return fields
-
-    def get_cards(self) -> Dict[int, Card]:
-        '''Lists cards (questions) in Metabase.'''
-        cards = self.get(endpoint='card')
-
-        # TODO: How to deal with it?
-        for card in cards:
-            if 'last-edit-info' in card:
-                card.pop('last-edit-info')
-
-        # TODO: Can be question, model or metric
-        cards = [
-            Card(**card) for card in cards
-            if card['type'] == 'question'
-        ]
-        cards = {card.id: card for card in cards}
-        return cards
-
-    def get_card(self, card_id):
-        return self.get(endpoint=f'card/{card_id}')
